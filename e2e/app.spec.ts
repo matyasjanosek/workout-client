faead4c2abc587f7cefcefffb93d7bcc07363f50
matyasjanosek <matyas.janosek@gmail.com>
2026-04-09 14:47:56 +0200

    fix playwright tests

--- e2e/app.spec.ts
+++ e2e/app.spec.ts
@@ -5,17 +5,18 @@ test('login page loads', async ({ page }) => {
     await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible()
 })
 
-test('register new user and login', async ({ page }) => {
+test('register new user and login', async ({ page }, testInfo) => {
+    const username = `pwuser${Date.now()}`
     await page.goto('https://deluxe-taiyaki-6e4f15.netlify.app/register', { timeout: 60000 })
-    await page.fill('input[placeholder="Username"]', 'playwrightuser5')
+    await page.fill('input[placeholder="Username"]', username)
     await page.fill('input[placeholder="Password"]', 'test123')
     await page.click('button')
-    await expect(page.getByText('Registered')).toBeVisible({ timeout: 60000 })
+    await expect(page.getByText('Go to login')).toBeVisible({ timeout: 60000 })
 })
 
 test('login redirects to workouts page', async ({ page }) => {
     await page.goto('https://deluxe-taiyaki-6e4f15.netlify.app', { timeout: 60000 })
-    await page.fill('input[placeholder="Username"]', 'playwrightuser5')
+    await page.fill('input[placeholder="Username"]', 'matyas2')
     await page.fill('input[placeholder="Password"]', 'test123')
     await page.click('button')
     await expect(page.getByRole('heading', { name: 'My Workouts' })).toBeVisible({ timeout: 60000 })
